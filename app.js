fc0cabda46bf3683516740f16e0f64bae6d940c3
Monday Victor A <vkingmaker1@gmail.com>
2019-08-01 10:33:24 +0100

    construct a message from the retrieved data

--- app.js
+++ app.js
@@ -2,7 +2,12 @@ const request = require('request');
 
 const url = 'https://api.darksky.net/forecast/0eee3d1a4dc06c1a19126d9b9bf573c2/37.8267,-122.4233' ;
 
-request({url}, (error, res) => {
-    const data = JSON.parse(res.body);
-    console.log(data);   
+request({url, json:true}, (error, res) => {
+    const {temperature} = res.body.currently;
+    const {precipProbability} = res.body.currently;
+
+    console.log(temperature);   
+    console.log(precipProbability);   
+
+    console.log(`It is currently ${temperature} degress out. There is a ${precipProbability}% chance of rain.`)
 });
