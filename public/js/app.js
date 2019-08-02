0f174bb4b530a3bcc115cad35e00ba1ea33d594f
Monday Victor A <vkingmaker1@gmail.com>
2019-08-02 23:44:20 +0100

    integrate UI and the server using fetch api

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,8 @@
 /* eslint-disable no-undef */
-const weatherForm = document.querySelector('form')
-const search = document.querySelector('input')
-const messageOne = document.querySelector('#message-1')
-const messageTwo = document.querySelector('#message-2')
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+const messageOne = document.querySelector('#message-1');
+const messageTwo = document.querySelector('#message-2');
 
 weatherForm.addEventListener('submit', (e) => {
   e.preventDefault();
