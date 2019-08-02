4574678559cf51c5e2c14b54cbbce1941e2073ca
Monday Victor A <vkingmaker1@gmail.com>
2019-08-02 15:55:11 +0100

    utiize the power of object destructuring

--- app.js
+++ app.js
@@ -6,17 +6,17 @@ const address = process.argv[2];
 if (!address) {
   console.log('Please provide an address');
 } else {
-  geocode(address, (err, data) => {
+  geocode(address, (err, { location, latitude, longitude }) => {
     if (err) {
       return console.log(err);
     }
 
-    forecast(data.latitude, data.longitude, (error, forecastData) => {
+    forecast(latitude, longitude, (error, forecastData) => {
       if (error) {
         return console.log(error);
       }
 
-      console.log(data.location);
+      console.log(location);
       console.log(forecastData);
     });
   });
--- utils/forecast.js
+++ utils/forecast.js
@@ -3,15 +3,15 @@ const request = require('request');
 const forecast = (latitude, longitude, callback) => {
   const url = `https://api.darksky.net/forecast/0eee3d1a4dc06c1a19126d9b9bf573c2/${latitude},${longitude}`;
 
-  request({ url, json: true }, (err, data) => {
+  request({ url, json: true }, (err, { body }) => {
     if (err) {
       callback('Unable to connect to weather service!', undefined);
-    } else if (data.body.error) {
+    } else if (body.error) {
       callback('Unable to find location.', undefined);
     } else {
-      const { temperature } = data.body.currently;
-      const { precipProbability } = data.body.currently;
-      const { summary } = data.body.daily.data[0];
+      const { temperature } = body.currently;
+      const { precipProbability } = body.currently;
+      const { summary } = body.daily.data[0];
       callback(undefined, `${summary} It is currently ${temperature} degress out. There is a ${precipProbability}% chance of rain .`);
     }
   });
