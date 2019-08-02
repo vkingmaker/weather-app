7ddddc9a20205dbd4efb49c118003e654c69db23
Monday Victor A <vkingmaker1@gmail.com>
2019-08-02 23:43:01 +0100

    integrate UI and the server using fetch api

--- app.js
+++ app.js
@@ -1,23 +1,102 @@
+const path = require('path');
+const express = require('express');
+const hbs = require('hbs');
 const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
-const address = process.argv[2];
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-if (!address) {
-  console.log('Please provide an address');
-} else {
-  geocode(address, (err, { location, latitude, longitude }) => {
-    if (err) {
-      return console.log(err);
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, 'public');
+const viewsPath = path.join(__dirname, './templates/views');
+const partialsPath = path.join(__dirname, './templates/partials');
+
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath));
+
+app.get('', (req, res) => {
+  res.render('index', {
+    title: 'Weather',
+    name: 'Monday Victor',
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About Me',
+    name: 'Monday Victor',
+  });
+});
+
+app.get('/help', (req, res) => {
+  res.render('help', {
+    helpText: 'This is some helpful text.',
+    title: 'Help',
+    name: 'Monday Victor',
+  });
+});
+
+app.get('/weather', (req, res) => {
+  if (!req.query.address) {
+    return res.send({
+      error: 'You must provide an address!',
+    });
+  }
+
+  geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
+    if (error) {
+      return res.send({ error });
     }
 
-    forecast(latitude, longitude, (error, forecastData) => {
-      if (error) {
-        return console.log(error);
+    forecast(latitude, longitude, (err, forecastData) => {
+      if (err) {
+        return res.send({ err });
       }
 
-      console.log(location);
-      console.log(forecastData);
+      res.send({
+        forecast: forecastData,
+        location,
+        address: req.query.address,
+      });
     });
   });
-}
+});
+
+app.get('/products', (req, res) => {
+  if (!req.query.search) {
+    return res.send({
+      error: 'You must provide a search term',
+    });
+  }
+
+  console.log(req.query.search);
+  res.send({
+    products: [],
+  });
+});
+
+app.get('/help/*', (req, res) => {
+  res.render('404', {
+    title: '404',
+    name: 'Monday Victor',
+    errorMessage: 'Help article not found.',
+  });
+});
+
+app.get('*', (req, res) => {
+  res.render('404', {
+    title: '404',
+    name: 'Monday Victor',
+    errorMessage: 'Page not found.',
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`The server is started at ${PORT}`);
+});
